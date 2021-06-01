b962b074bda198ddb6b0c57f3019c32054322215
Rafael Carvalho <rafaeloc.dev@gmail.com>
2021-06-01 09:00:52 -0300

    Html e CSS
    Finalização do HTML e CSS

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,1 +1,35 @@
-﻿
+let botaoCalcular = document.getElementById('btnCalcular');
+
+function calculandoIMC() {
+    let peso = document.getElementById('peso').value;
+    let altura = document.getElementById('altura').value / 100;
+    let imcCalculado = document.getElementById('imc-calculado');
+    let classificacaoIMC = document.getElementById('mensagem-imc')
+    let tabIMC = [{ valor: 18.5, classificacao: 'Abaixo do peso' },
+    { valor: 25, classificacao: 'Peso normal' },
+    { valor: 30, classificacao: 'Sobrepeso' },
+    { valor: 35, classificacao: 'Obesidade grau I' },
+    { valor: 40, classificacao: 'Obesidade grau II' },
+    { valor: 200, classificacao: 'Obesidade grau III ou mórbida' }
+    ]
+
+    if (peso == '' || altura == '') {
+        imcCalculado.textContent = ''
+        classificacaoIMC.textContent = 'Informe seu peso e altura';
+    } else {
+        let imc = (peso / altura ** 2).toFixed(1);
+        for (let i = 0; i < tabIMC.length; i++) {
+            if (imc < tabIMC[i].valor) {
+                imcCalculado.textContent = imc;
+                classificacaoIMC.textContent = tabIMC[i].classificacao;
+                i = 10;
+            }
+        }
+    }
+}
+
+function slider() {
+
+}
+
+botaoCalcular.addEventListener('click', calculandoIMC);
